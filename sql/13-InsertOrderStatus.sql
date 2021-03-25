b39e8be0bf4af8c47b89c7a4990311f2457190a5
Joachim Abitan <Joachim.Abitan@bastide-medical.fr>
2021-03-25 10:28:10 +0100

    add sql order status

---
+++ sql/13-InsertOrderStatus.sql
@@ -0,0 +1,5 @@
+INSERT INTO orderStatus (label, sapStatus)  values ('En attente', 'A');
+INSERT INTO orderStatus (label, sapStatus)  values ('En cours de traitement', 'AA');
+INSERT INTO orderStatus (label, sapStatus)  values ('Livrée partiellement', 'B');
+INSERT INTO orderStatus (label, sapStatus)  values ('Livrée', 'C');
+INSERT INTO orderStatus (label, sapStatus)  values ('Facturée', 'D');
--- sql/13-revertAddOrderStatus.sql
+++ sql/13-revertAddOrderStatus.sql
@@ -1,9 +1,11 @@
 ALTER TABLE boncde_entete DROP FOREIGN KEY fk_status_id;
 ALTER TABLE boncde_entete DROP INDEX fk_status_id;
 ALTER TABLE boncde_entete DROP COLUMN statusId;
+ALTER TABLE boncde_entete DROP COLUMN dateStatus;
 
 ALTER TABLE bonrec_materiel DROP FOREIGN KEY fk_bonrec_status_id;
 ALTER TABLE bonrec_materiel DROP INDEX fk_bonrec_status_id;
 ALTER TABLE bonrec_materiel DROP COLUMN statusId;
+ALTER TABLE bonrec_materiel DROP COLUMN dateStatus;
 
 DROP TABLE orderStatus;
