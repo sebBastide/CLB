35e5c1e10129a402e9c6454d675676cb4fe7ce9d
Joachim Abitan <Joachim.Abitan@bastide-medical.fr>
2021-04-26 14:50:48 +0200

    bf(NA/Products): order and uppercase products

---
+++ sql/na-uppercase-order-products.sql
@@ -0,0 +1,5 @@
+UPDATE produit_had SET otherLabel = 'LOCATION MATELAS A AIR CLASSE 4 (CURATIF)', lb_classement = '5' where otherLabel LIKE 'Location matelas à air classe 4 (curatif)';
+UPDATE produit_had SET otherLabel = 'ACHAT MATELAS MEMOIRE DE FORME CLASSE 3', lb_classement = '3' where otherLabel LIKE 'Achat matelas mémoire de forme classe 3';
+UPDATE produit_had SET otherLabel = 'LOCATION MATELAS A AIR CLASSE 3 (PREVENTIF)', lb_classement = '4' where otherLabel LIKE 'Location matelas à air classe 3 (préventif)';
+UPDATE produit_had SET otherLabel = 'ACHAT MATELAS MEMOIRE DE FORME CLASSE 2', lb_classement = '2' where otherLabel LIKE 'Achat matelas mémoire de forme classe 2';
+UPDATE produit_had SET lb_produit = 'COUSSIN ANTI ESCARRE' where lb_produit LIKE 'Coussin anti escarre';
