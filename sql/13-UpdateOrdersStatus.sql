d7c6321522fc14635ca750522f850bb7ff4d6b39
Joachim Abitan <Joachim.Abitan@bastide-medical.fr>
2021-05-05 14:40:11 +0200

    delivery preparation

--- sql/13-UpdateOrdersStatus.sql
+++ sql/13-UpdateOrdersStatus.sql
@@ -1,3 +1,3 @@
-UPDATE boncde_entete SET statusId = 5, dateStatus = '2021-03-01 00:00:00' where credat < '2021-03-01';
+UPDATE boncde_entete SET statusId = 5, dateStatus = '2021-03-15 00:00:00' where credat < '2021-03-15';
 
-UPDATE bonrec_materiel SET statusId = 5, dateStatus = '2021-03-01 00:00:00' where credat < '2021-03-01';
+UPDATE bonrec_materiel SET statusId = 5, dateStatus = '2021-03-15 00:00:00' where credat < '2021-03-15';
--- sql/13-addOrderStatus.sql
+++ sql/13-addOrderStatus.sql
@@ -3,7 +3,7 @@ CREATE TABLE orderStatus
 	id INT(6) AUTO_INCREMENT PRIMARY KEY,
 	label VARCHAR(50) NOT NULL,
 	sapStatus VARCHAR(2)
-);
+) ENGINE=INNODB;
 
 ALTER TABLE boncde_entete ADD COLUMN statusId INT(6) DEFAULT NULL;
 ALTER TABLE boncde_entete ADD CONSTRAINT fk_status_id FOREIGN KEY (statusId) REFERENCES orderStatus(id);
